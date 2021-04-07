55e8079aaee862e4c3595fc6991f045d95c5b6ab
Drew Thomas <cdthomas.92@gmail.com>
2021-04-08 07:57:30 +1000

    14.5 - 14.6

--- src/chunk.h
+++ src/chunk.h
@@ -2,9 +2,11 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 // OpCode is the kind of instruction.
 typedef enum {
+  OP_CONSTANT, // produces a constant
   OP_RETURN, // return from the current function
 } OpCode;
 
@@ -13,10 +15,13 @@ typedef struct {
   int count; // the number of allocated entries that are in use
   int capacity; // the number of elements allocated for the array
   uint8_t* code; // dynamic array of bytes
+  int* lines; // dynamic array of line numbers for debugging runtime errors
+  ValueArray constants; // dynamic array of constants
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
