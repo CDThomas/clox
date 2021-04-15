ffc075e51619b87a353bb4748d11779de5be7040
Drew Thomas <cdthomas.92@gmail.com>
2021-04-15 19:40:33 +1000

    16.2 - 16.3

--- src/compiler.c
+++ src/compiler.c
@@ -7,4 +7,19 @@
 
 void compile(const char* source) {
   initScanner(source);
+  int line = -1;
+  for (;;) {
+    Token token = scanToken();
+    if (token.line != line) {
+      printf("%4d ", token.line);
+      line = token.line;
+    } else {
+      printf("   | ");
+    }
+    // `*s` allows us to pass in the precision (where to stop printing the string). We need this since
+    // the original string doesn't have a terminator at the end.
+    printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+    if (token.type == TOKEN_EOF) break;
+  }
 }
