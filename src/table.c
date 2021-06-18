1d71fa06a27dc420b4d8500a33dbdc21b030898c
Drew Thomas <cdthomas.92@gmail.com>
2021-06-18 18:48:52 +1000

    20.1 - 20.4.2 (doesn't compile)

---
+++ src/table.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "memory.h"
+#include "object.h"
+#include "table.h"
+#include "value.h"
+
+#define TABLE_MAX_LOAD 0.75
+
+void initTable(Table* table) {
+    table->count = 0;
+    table->capacity = 0;
+    table->entries = NULL;
+}
+
+void freeTable(Table* table) {
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    initTable(table);
+}
+
+static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+    uint32_t index = key->hash % capacity;
+    for (;;) {
+        Entry* entry = &entries[index];
+        if (entry->key == key || entry->key == NULL) {
+            return entry;
+        }
+
+        index = (index + 1) % capacity;
+    }
+}
+
+// Pick up at 20.4.3 Allocating and resizing
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+    // Grow array when table is at least 75% full
+    if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+      int capacity = GROW_CAPACITY(table->capacity);
+      adjustCapacity(table, capacity);
+    }
+
+    Entry* entry = findEntry(table->entries, table->capacity, key);
+    bool isNewKey = entry->key == NULL;
+    if (isNewKey) table->count++;
+
+    entry->key = key;
+    entry->value = value;
+    return isNewKey;
+}
