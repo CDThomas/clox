da6b51bec231c445832b1814a4e4726e2697598a
Drew Thomas <cdthomas.92@gmail.com>
2021-06-27 18:09:58 +1000

    Implement verifying runtime error expecations

--- scripts/test.py
+++ scripts/test.py
@@ -7,13 +7,6 @@ import sys
 import typing
 
 # TODO:
-# - Support expecting errors
-#    - Compilation errors: https://github.com/munificent/craftinginterpreters/blob/master/test/assignment/grouping.lox
-#      - Line number is optional https://github.com/munificent/craftinginterpreters/blob/3e5f0fa6636b68eddfe4dc0173af95130403f961/test/unexpected_character.lox
-#    - Runtime errors: https://github.com/munificent/craftinginterpreters/blob/master/test/assignment/undefined.lox
-#    - can have both runtime error expectations and normal expectations (https://github.com/munificent/craftinginterpreters/blob/3e5f0fa6636b68eddfe4dc0173af95130403f961/test/super/extra_arguments.lox)
-#    - can only have one runtime error expectation
-#    - can't have output/runtime error expectations if there are compilation error expectations
 # - Support only running certain tests
 # - Support choosing an interpreter
 
@@ -21,17 +14,20 @@ import typing
 TEST_PATH: typing.Final = "test/**/*.lox"
 
 EXPECTED_OUTPUT_REGEX: typing.Final = r"// expect: ?(.*)"
-
 EXPECTED_SYNTAX_ERROR_REGEX: typing.Final = (
     r"// (?:\[line (?P<line_number>\d+)\] )?(?P<error>Error.+)"
 )
-
 EXPECTED_RUNTIME_ERROR_REGEX: typing.Final = r"// expect runtime error: (.+)"
-
 SYNTAX_ERROR_REGEX: typing.Final = r"^\[line \d+\] Error.+"
+STACK_TRACE_REGEX: typing.Final = r"^\[line (\d+)\]"
 
 
 class OutputExpectation(typing.NamedTuple):
+    expected: str
+    line_number: int
+
+
+class RuntimeErrorExpectation(typing.NamedTuple):
     expected: str
     line_number: int
 
@@ -41,7 +37,11 @@ class SyntaxErrorExpectation(typing.NamedTuple):
     line_number: int
 
 
-Expectation = typing.Union[OutputExpectation, SyntaxErrorExpectation]
+Expectation = typing.Union[
+    OutputExpectation,
+    RuntimeErrorExpectation,
+    SyntaxErrorExpectation,
+]
 
 
 class Failure(typing.NamedTuple):
@@ -112,7 +112,8 @@ def parse_expectation(
     if match := re.search(EXPECTED_OUTPUT_REGEX, line):
         expected = match.group(1)
         return OutputExpectation(expected=expected, line_number=line_number)
-    if match := re.search(EXPECTED_SYNTAX_ERROR_REGEX, line):
+
+    elif match := re.search(EXPECTED_SYNTAX_ERROR_REGEX, line):
         line_number_match = match.groupdict()["line_number"]
         expected_line_number = (
             int(line_number_match) if line_number_match else line_number
@@ -121,16 +122,28 @@ def parse_expectation(
         return SyntaxErrorExpectation(
             expected=expected, line_number=expected_line_number
         )
+
+    elif match := re.search(EXPECTED_RUNTIME_ERROR_REGEX, line):
+        expected = match.group(1)
+        return RuntimeErrorExpectation(
+            expected=expected, line_number=line_number
+        )
+
     else:
         return None
 
 
 def validate_expectations(
     output_expectations: list[OutputExpectation],
+    runtime_error_expectations: list[RuntimeErrorExpectation],
     syntax_error_expectations: list[SyntaxErrorExpectation],
 ) -> list[Failure]:
     if syntax_error_expectations and output_expectations:
         return [Failure("Can't expect both syntax errors and output")]
+    if syntax_error_expectations and runtime_error_expectations:
+        return [Failure("Can't expect both syntax errors and runtime errors.")]
+    elif len(runtime_error_expectations) > 1:
+        return [Failure("Can't expect more than one runtime error.")]
     else:
         return []
 
@@ -156,6 +169,8 @@ def verify_syntax_error_expectations(
                 failure = Failure(f"Unexpected syntax error: {error_line}")
                 failures.append(failure)
         elif error_line != "":
+            # TODO: this fallthrough is confusing since it handles unexpected
+            # runtime errors too. Should prob move this up a level.
             failure = Failure(f"Unexpected output on stderr: {error_line}")
             failures.append(failure)
 
@@ -166,9 +181,60 @@ def verify_syntax_error_expectations(
     return failures
 
 
+def verify_runtime_error_expectations(
+    error_lines: list[str],
+    # TODO: don't really need to use a list for runtime_error_expectations here
+    runtime_error_expectations: list[RuntimeErrorExpectation],
+) -> list[Failure]:
+    runtime_error_expectation = runtime_error_expectations[0]
+    if len(error_lines) < 2:
+        message = (
+            "Expected runtime error "
+            f"'{runtime_error_expectation.expected}' and got none."
+        )
+        return [Failure(message)]
+
+    failures: list[Failure] = []
+
+    if runtime_error_expectation.expected != error_lines[0]:
+        message = (
+            "Expected runtime error "
+            f"'{runtime_error_expectation.expected}' "
+            f"and got '{error_lines[0]}'"
+        )
+        failures.append(Failure(message))
+
+    stack_trace_lines = error_lines[1:]
+
+    stack_trace_match = next(
+        stack_trace_match
+        for line in stack_trace_lines
+        if (stack_trace_match := re.search(STACK_TRACE_REGEX, line))
+    )
+
+    if not stack_trace_match:
+        failures.append(
+            Failure(f"Expected stack trace and got:\n{stack_trace_lines}")
+        )
+
+    actual_stack_trace_line_number = int(stack_trace_match.group(1))
+    expected_stack_trace_line_number = runtime_error_expectation.line_number
+
+    if actual_stack_trace_line_number != expected_stack_trace_line_number:
+        message = (
+            f"Expected runtime error on line {actual_stack_trace_line_number}"
+            f" but was on line {expected_stack_trace_line_number}."
+        )
+        failures.append(Failure(message))
+
+    return failures
+
+
 def verify_output_expectations(
     output_lines: list[str], output_expectations: list[OutputExpectation]
 ) -> list[Failure]:
+    # TODO: it's possibly confusing that this is here vs in the
+    # validate_expectations func
     if len(output_lines) > len(output_expectations):
         return [Failure("Recieved extra output on stdout.")]
 
@@ -202,40 +268,62 @@ def verify_expectations(
     expectations: list[Expectation],
 ) -> list[Failure]:
     output_expectations: list[OutputExpectation] = []
+    runtime_error_expectations: list[RuntimeErrorExpectation] = []
     syntax_error_expectations: list[SyntaxErrorExpectation] = []
 
     for expectation in expectations:
         if type(expectation) is OutputExpectation:
             output_expectations.append(expectation)
-        if type(expectation) is SyntaxErrorExpectation:
+        elif type(expectation) is RuntimeErrorExpectation:
+            runtime_error_expectations.append(expectation)
+        elif type(expectation) is SyntaxErrorExpectation:
             syntax_error_expectations.append(expectation)
 
     output_lines = stdout.splitlines()
     error_lines = stderr.splitlines()
 
     validation_failures = validate_expectations(
-        output_expectations, syntax_error_expectations
+        output_expectations,
+        runtime_error_expectations,
+        syntax_error_expectations,
     )
 
     if validation_failures:
         return validation_failures
 
-    if syntax_error_expectations:
+    expected_exit_code: int
+    if runtime_error_expectations:
+        expected_exit_code = 70
+    elif syntax_error_expectations:
+        expected_exit_code = 65
+    else:
+        expected_exit_code = 0
+
+    exit_code_failures = verify_exit_code(exit_code, expected_exit_code)
+
+    runtime_error_expectation_failures: list[Failure] = []
+
+    if runtime_error_expectations:
+        runtime_error_expectation_failures = verify_runtime_error_expectations(
+            error_lines, runtime_error_expectations
+        )
+    else:
         syntax_error_expectation_failures = verify_syntax_error_expectations(
             error_lines, syntax_error_expectations
         )
 
-        exit_code_failures = verify_exit_code(exit_code, 65)
-
-        return syntax_error_expectation_failures + exit_code_failures
+        if syntax_error_expectation_failures:
+            return syntax_error_expectation_failures + exit_code_failures
 
     output_expectation_failures = verify_output_expectations(
         output_lines, output_expectations
     )
 
-    exit_code_failures = verify_exit_code(exit_code, 0)
-
-    return output_expectation_failures + exit_code_failures
+    return (
+        output_expectation_failures
+        + runtime_error_expectation_failures
+        + exit_code_failures
+    )
 
 
 def run_test(path: str) -> Test:
