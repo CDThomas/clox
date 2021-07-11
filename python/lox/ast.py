36b46119a47eb39001bc84344ff3ff95a938651b
Drew Thomas <cdthomas.92@gmail.com>
2021-07-11 13:52:28 +1000

    Chapter 7 (#10)

--- python/lox/ast.py
+++ python/lox/ast.py
@@ -1,7 +1,12 @@
 import dataclasses
+import typing
+
+import lark
 from lark import ast_utils
 
 # TODO: type hints for this module
+
+Value = typing.Union[str, float, bool]
 
 
 class _Ast(ast_utils.Ast):
@@ -13,35 +18,35 @@ class _Ast(ast_utils.Ast):
 class _Expression(_Ast):
     # This will be skipped by create_transformer() because it starts with an
     # underscore.
-    pass
+    def accept(self, visitor):
+        pass
 
 
 @dataclasses.dataclass
 class Literal(_Expression):
-    # TODO: better type for value
-    value: object
+    value: typing.Optional[Value]
 
     def accept(self, visitor):
-        return visitor.visitLiteralExpression(self)
+        return visitor.visit_literal_expression(self)
 
 
 @dataclasses.dataclass
 class Unary(_Expression):
-    operator: str
+    operator: lark.Token
     right: _Expression
 
     def accept(self, visitor):
-        return visitor.visitUnaryExperssion(self)
+        return visitor.visit_unary_expression(self)
 
 
 @dataclasses.dataclass
 class Binary(_Expression):
     left: _Expression
-    operator: str
+    operator: lark.Token
     right: _Expression
 
     def accept(self, visitor):
-        return visitor.visitBinaryExperssion(self)
+        return visitor.visit_binary_expression(self)
 
 
 @dataclasses.dataclass
@@ -49,4 +54,4 @@ class Grouping(_Expression):
     expression: _Expression
 
     def accept(self, visitor):
-        return visitor.visitGroupingExperssion(self)
+        return visitor.visit_grouping_expression(self)
---
+++ python/lox/interpreter.py
@@ -0,0 +1,153 @@
+import lark
+import typing
+
+from lox import ast
+
+Value = typing.Union[str, float, bool]
+
+
+class LoxRuntimeError(Exception):
+    def __init__(self, token: lark.Token, message: str):
+        self.token = token
+        self.message = message
+
+
+class Interpreter:
+    def interpret(self, expression: ast._Expression):
+        value = self._evaluate(expression)
+        return self._stringify(value)
+
+    def visit_literal_expression(
+        self, expression: ast.Literal
+    ) -> typing.Optional[Value]:
+        return expression.value
+
+    def visit_grouping_expression(
+        self, expression: ast.Grouping
+    ) -> typing.Optional[Value]:
+        return self._evaluate(expression.expression)
+
+    def visit_unary_expression(
+        self, expression: ast.Unary
+    ) -> typing.Optional[Value]:
+        right = self._evaluate(expression.right)
+
+        if expression.operator.value == "!":
+            return not self._is_truthy(right)
+        elif expression.operator.value == "-":
+            right = self._check_number_operand(expression.operator, right)
+            return -right
+
+        # Unreachable.
+        return None
+
+    def visit_binary_expression(
+        self, expression: ast.Binary
+    ) -> typing.Optional[Value]:
+        left = self._evaluate(expression.left)
+        right = self._evaluate(expression.right)
+
+        op = expression.operator.value
+
+        if op == ">":
+            left, right = self._check_number_operands(
+                expression.operator, left, right
+            )
+            return left > right
+        elif op == ">=":
+            left, right = self._check_number_operands(
+                expression.operator, left, right
+            )
+            return left >= right
+        elif op == "<":
+            left, right = self._check_number_operands(
+                expression.operator, left, right
+            )
+            return left < right
+        elif op == "<=":
+            left, right = self._check_number_operands(
+                expression.operator, left, right
+            )
+            return left <= right
+        elif op == "-":
+            left, right = self._check_number_operands(
+                expression.operator, left, right
+            )
+            return left - right
+        elif op == "/":
+            left, right = self._check_number_operands(
+                expression.operator, left, right
+            )
+            return left / right
+        elif op == "*":
+            left, right = self._check_number_operands(
+                expression.operator, left, right
+            )
+            return left * right
+        elif op == "==":
+            return left == right
+        elif op == "+":
+            if isinstance(left, float) and isinstance(right, float):
+                return left + right
+
+            if isinstance(left, str) and isinstance(right, str):
+                return left + right
+
+            raise LoxRuntimeError(
+                expression.operator,
+                "Operands must be two numbers or two strings.",
+            )
+
+        # Unreachable.
+        return None
+
+    def _evaluate(self, expression: ast._Expression) -> typing.Optional[Value]:
+        return expression.accept(self)
+
+    def _is_truthy(self, value: typing.Optional[Value]) -> bool:
+        if value is None:
+            return False
+
+        if isinstance(value, bool):
+            return value
+
+        return True
+
+    def _stringify(self, value: typing.Optional[Value]) -> str:
+        if value is None:
+            return "nil"
+
+        if value is True:
+            return "true"
+
+        if value is False:
+            return "false"
+
+        if isinstance(value, float):
+            text = str(value)
+
+            if text.endswith(".0"):
+                return text[:-2]
+            else:
+                return text
+
+        return value
+
+    def _check_number_operand(
+        self, operator: lark.Token, operand: typing.Optional[Value]
+    ) -> float:
+        if isinstance(operand, float):
+            return operand
+
+        raise LoxRuntimeError(operator, "Operand must be a number.")
+
+    def _check_number_operands(
+        self,
+        operator: lark.Token,
+        left: typing.Optional[Value],
+        right: typing.Optional[Value],
+    ) -> tuple[float, float]:
+        if isinstance(left, float) and isinstance(right, float):
+            return left, right
+
+        raise LoxRuntimeError(operator, "Operands must be numbers.")
