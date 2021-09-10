99673bc874d14d6c93acdd0c907326bb8154a79a
Drew Thomas <cdthomas.92@gmail.com>
2021-09-10 16:41:49 +1000

    12.4

--- python/lox/lox_instance.py
+++ python/lox/lox_instance.py
@@ -1,5 +1,10 @@
 import typing
 
+import lark
+
+from lox import errors
+from lox import types
+
 if typing.TYPE_CHECKING:
     from lox import lox_class
 
@@ -7,6 +12,18 @@ if typing.TYPE_CHECKING:
 class LoxInstance:
     def __init__(self, klass: "lox_class.LoxClass") -> None:
         self.klass = klass
+        self.fields: dict[str, typing.Optional[types.Value]] = {}
+
+    def get(self, name: lark.Token) -> typing.Optional[types.Value]:
+        if name.value in self.fields:
+            return self.fields[name.value]
+
+        raise errors.LoxRuntimeError(name, f"Undefined property {name.value}.")
+
+    def set(
+        self, name: lark.Token, value: typing.Optional[types.Value]
+    ) -> None:
+        self.fields[name.value] = value
 
     def to_string(self) -> str:
         return f"{self.klass.name} instance"
