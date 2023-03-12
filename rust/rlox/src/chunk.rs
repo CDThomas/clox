837707b7120a168ae24a86fa7b88359bc2052020
Drew Thomas <cdthomas.92@gmail.com>
2023-03-12 13:47:55 +1100

    14.5-14.6: add constant instructions and add line numbers to disassembler

--- rust/rlox/src/chunk.rs
+++ rust/rlox/src/chunk.rs
@@ -1,4 +1,7 @@
+use crate::value::Value;
+
 pub enum OpCode {
+    OpConstant,
     OpReturn,
 }
 
@@ -14,6 +17,7 @@ impl TryFrom<u8> for OpCode {
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
             value if value == OpCode::OpReturn as u8 => Ok(OpCode::OpReturn),
+            value if value == OpCode::OpConstant as u8 => Ok(OpCode::OpConstant),
             _ => Err(format!(
                 "Failed to convert u8 to OpCode. No OpCode matches value: {value}"
             )),
@@ -23,7 +27,9 @@ impl TryFrom<u8> for OpCode {
 
 #[derive(Debug, Default)]
 pub struct Chunk {
-    code: Vec<u8>,
+    pub code: Vec<u8>,
+    pub constants: Vec<Value>,
+    pub lines: Vec<usize>,
 }
 
 impl Chunk {
@@ -31,8 +37,9 @@ impl Chunk {
         Chunk::default()
     }
 
-    pub fn write_byte(&mut self, byte: impl Into<u8>) {
+    pub fn write_byte(&mut self, byte: impl Into<u8>, line: usize) {
         self.code.push(byte.into());
+        self.lines.push(line);
     }
 
     pub fn disassemble(&self, name: &str) {
@@ -44,8 +51,20 @@ impl Chunk {
         }
     }
 
+    pub fn add_constant(&mut self, constant: impl Into<Value>) -> usize {
+        self.constants.push(constant.into());
+        self.constants.len() - 1
+    }
+
     fn disassemble_instruction(&self, offset: usize) -> usize {
         print!("{offset:04} ");
+
+        let line = self.lines[offset];
+        if offset > 0 && line == self.lines[offset - 1] {
+            print!("   | ");
+        } else {
+            print!("{line:4} ");
+        }
 
         let instruction: OpCode = self.code[offset]
             .try_into()
@@ -53,6 +72,7 @@ impl Chunk {
 
         match instruction {
             OpCode::OpReturn => simple_instruction("OP_RETURN", offset),
+            OpCode::OpConstant => constant_instruction("OP_CONSTANT", self, offset),
         }
     }
 }
@@ -60,4 +80,13 @@ impl Chunk {
 fn simple_instruction(name: &str, offset: usize) -> usize {
     println!("{name}");
     offset + 1
+}
+
+fn constant_instruction(name: &str, chunk: &Chunk, offset: usize) -> usize {
+    let constant = chunk.code[offset + 1] as usize;
+    let value = &chunk.constants[constant];
+
+    println!("{name:<16} {constant:>4} '{value}'");
+
+    offset + 2
 }
